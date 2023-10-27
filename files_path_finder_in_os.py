af11a8266d102054b08a7d90965032ddfb3e3dc8
VIDAPANAKAL GANGAPPAGARI YASHWANTH KUMAR <83589480+vgyashwanth@users.noreply.github.com>
2023-10-28 00:59:36 +0530

    Create files_path_finder_in_os.py

---
+++ files_path_finder_in_os.py
@@ -0,0 +1,30 @@
+import os
+import platform as  pt
+import  string
+import sys
+print(pt.system())
+#sys.exit()
+req_file = input("enter your file name: ")
+if "Windows"==pt.system(): 
+     drive = string.ascii_uppercase #return all characters in  upper case as string
+     drive_list = []
+     for x in drive:
+          if os.path.exists(x+":\\"):
+               drive_list.append(x+":\\") #adding directroy if exit
+     print(drive_list)   
+     for each_driver in  drive_list: #searching path
+          for path,dic,file in (os.walk(each_driver)):
+               for each_file in file:
+                    if(req_file==each_file): 
+                         print(os.path.join(path,each_file)) #print path
+else: #for linux operating system
+     for path,dic,file in os.walk("/"):
+          for each_file in file:
+               if(req_file==each_file):
+                    print(os.path.join(path,each_file))
+
+
+                    
+               
+                         
+
