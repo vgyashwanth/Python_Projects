652a1b6200861d6d5a934efe22cc573867f747cc
VIDAPANAKAL GANGAPPAGARI YASHWANTH KUMAR <83589480+vgyashwanth@users.noreply.github.com>
2023-10-28 12:14:07 +0530

    Update dir_files_path_seperator.py

--- dir_files_path_seperator.py
+++ dir_files_path_seperator.py
@@ -7,18 +7,18 @@ if(os.path.exists(path)):
     for dir_file in os.listdir(path):
         new_path = os.path.join(path,dir_file)
         if(os.path.isdir(new_path)):
-            my_dir.append(new_path)
+            my_dir.append(new_path) #storing directories
         elif(os.path.isfile(new_path)): 
-            my_file.append(new_path)    
-    print(f"Number of Directories are: {len(my_dir)} ")
+            my_file.append(new_path) #storing files
+    print(f"Number of Directories are: {len(my_dir)} ") # return the number of directories
     for dir in my_dir:
         print(dir)
-    print(f"Number of Files are: {len(my_file)} ")
+    print(f"Number of Files are: {len(my_file)} ") # return the number of files
     for file  in my_file:
         print(file)
 
 else:
     print("Path not Exists enter the valid path")
-    sys.exit()        
+    sys.exit()        # stop the interpreter to excute the python code
          
                 
